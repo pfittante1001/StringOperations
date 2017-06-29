3ac26b60cc66bb5b009007c98ea34eac435787aa
pfittante <pfittante@yahoo.com>
2017-06-29 14:13:19 -0400

    String Operations

--- StringOperations/Program.cs
+++ StringOperations/Program.cs
@@ -10,6 +10,33 @@ namespace StringOperations
     {
         static void Main(string[] args)
         {
+            /*
+            string sound = "Bong";
+
+            Console.WriteLine("\"" + sound + "\"");
+            Console.WriteLine("\a");
+            Console.WriteLine("\t Pete \n \t Fittante");
+            */
+
+            string word1 = "Apple";
+            string word2 = "Apple";
+            int stLen = word1.Length;
+            string examp = word1 + " " + word2;
+
+            bool equal = word1 == word2;
+            bool equal2 = word1.Equals(word2);
+
+            Console.WriteLine(equal + " " + equal2);
+            Console.WriteLine(word1.Length);
+            Console.WriteLine(stLen);
+            Console.WriteLine(examp);
+
+            for (int i = 0; i < stLen; i++)
+            {
+                Console.Write(word1[i]);
+            }
+
+
         }
     }
 }
